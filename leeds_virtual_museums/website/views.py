4ff9eae4d476cbb28568060cbae55a0fa7cc9cf0
刘钰雯 <evian@liuyuwendeMacBook-Air.local>
2022-08-16 02:59:15 +0100

    add logout

--- leeds_virtual_museums/website/views.py
+++ leeds_virtual_museums/website/views.py
@@ -6,36 +6,40 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def login_view(request, *args, **kwargs):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("/")
-        else:
-            return render(request, "login.html", {"status": "Authentication failed."})
+        if request.POST.get("logout"):
+            logout(request)
+            return redirect("/login")
+        elif request.POST.get("login"):
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                return render(request, "login.html", {"status": "Authentication failed."})
     return render(request, "login.html", {})
 
 @login_required(login_url="/login")
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
-@login_required
+@login_required(login_url="/login")
 def account_view(request, *args, **kwargs):
     return render(request, "account.html", {})
 
-@login_required
+@login_required(login_url="/login")
 def curriculum_view(request, *args, **kwargs):
     return render(request, "curriculum.html", {})
 
-@login_required
+@login_required(login_url="/login")
 def evaluation_view(request, *args, **kwargs):
     return render(request, "evaluation.html", {})
 
-@login_required
+@login_required(login_url="/login")
 def evaluation_quiz_view(request, *args, **kwargs):
     return render(request, "evaluation_quiz.html", {})
 
-@login_required
+@login_required(login_url="/login")
 def plan_visit_view(request, *args, **kwargs):
     return render(request, "plan_visit.html", {})
